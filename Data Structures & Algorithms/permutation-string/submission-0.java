642cb299e360573c8eb2418150ce320b2e022881
Vatsal Raj <143717741+VatsalRaj481@users.noreply.github.com>
2026-09-07 15:33:09 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length())return false;
+        int[] freq = new int[26];
+        for(char c:s1.toCharArray()){
+            freq[c-'a']++;
+        }
+        int left=0;
+        for(int right=0;right<s2.length();right++){
+            freq[s2.charAt(right)-'a']--;
+            if(right-left+1>s1.length()){
+                freq[s2.charAt(left)-'a']++;
+                left++;
+            }
+            if(right-left+1==s1.length()){
+                boolean isPermutation=true;
+                for(int count:freq){
+                    if(count!=0){
+                        isPermutation=false;
+                        break;
+                    }
+                }
+                if(isPermutation)return true;
+            }
+        }
+        return false;
+    }
+}
